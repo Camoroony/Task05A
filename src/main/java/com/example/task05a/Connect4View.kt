da2ca6bd2b23aa9d89423af80de9a465f560e200
Cameron <cjlane9090@gmail.com>
2023-04-07 11:52:22 +0100

    Setup

--- src/main/java/com/example/task05a/Connect4View.kt
+++ src/main/java/com/example/task05a/Connect4View.kt
@@ -1,6 +1,7 @@
 package com.example.task05a
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -27,6 +28,52 @@ class Connect4View: View {
     private val noPlayerPaint: Paint= Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         color = Color.WHITE
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        val gridLeft: Float = 0f
+        val gridTop: Float = 0f
+        val gridRight: Float = gridLeft + colCount * (circleDiameter+circleSpacing) + circleSpacing
+        val gridBottom: Float = gridTop + rowCount * (circleDiameter+circleSpacing) + circleSpacing
+
+        //draw the game board
+        canvas?.drawRect(gridLeft, gridTop, gridRight, gridBottom, gridPaint)
+
+        super.onDraw(canvas)
+
+        fun drawCircle() {
+            val radius = circleDiameter / 2f
+
+            for (row in 0 until rowCount) {
+                // The vertical center is the same for each circle in the row
+                val cy = gridTop + circleSpacing + ((circleDiameter + circleSpacing) * row) + radius
+
+                for (col in 0 until colCount) {
+                    // We will later on want to use the game data to determine this
+                    val paint = noPlayerPaint
+
+                    // Drawing circles uses the center and radius
+                    val cx = gridLeft + circleSpacing + ((circleDiameter + circleSpacing) * col) + radius
+
+                    canvas?.drawCircle(cx, cy, radius, paint)
+                }
+            }
+        }
+
+    }
+
+
+
+    private fun recalculateDimensions(w: Int = width, h: Int = height) {
+        val diameterX = w/(colCount + (colCount+1)*circleSpacingRatio)
+        val diameterY = h/(rowCount + (rowCount+1)*circleSpacingRatio)
+
+        circleDiameter = minOf(diameterX, diameterY)
+        circleSpacing = circleDiameter*circleSpacingRatio
     }
 }
 
